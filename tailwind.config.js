c734f00ffcd9d6df3ccdd937be3652fd67e411e2
bolderstephanie <bolderstephanie@gmail.com>
2020-09-25 14:55:54 +0200

    halve pagina, niet responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,20 +2,26 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        display: ['Spartan', 'sans-serif'],
+        display: ['Josefin sans', 'sans-serif'],
         body: ['Graphik', 'sans-serif'],
-        titel: ['Dancing Script', 'serif'],
       } ,
       colors: {
         verydarkmagenda: '#502050' ,
         darkgrayishmagenda: '#937B92' ,
         lightgrayishmagenda: '#F7F2F7',
         softpink: '#EE68A4',
+        pink: '#9C5A4E' ,
         white: '#FFFFFF',
+        donkerzwart: '#4a4a4a' ,
+        buttonbeige: '#A88D6F' ,
+        lichtbeige: '#FFFAE2' ,
+        donkergrijs: '#080808' ,
+        lichtgrijs: '#9AA5A7' ,
+        achtergrondbeige: '#F6F5F1' ,
+        achtergrondgrijs: '#9AA5AB' ,
       }
     },
   },
   variants: {},
   plugins: [],
 }
-
